44a0b37759d763138c7c15aacab550d8c39b9895
LIy-hub <2734855720@qq.com>
2026-07-30 16:51:16 +0800

    Port LoliPickaxe to Minecraft 1.20.2

--- src/main/java/com/liymod/mixin/PlayerMixin.java
+++ src/main/java/com/liymod/mixin/PlayerMixin.java
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -63,14 +62,6 @@ public abstract class PlayerMixin extends LivingEntity {
                 || LoliProtection.isUntargetable(target)) {
             ci.cancel();
         }
-    }
-
-    @ModifyVariable(method = "setAbsorptionAmount", at = @At("HEAD"), argsOnly = true)
-    private float lolipickaxe$blockAbsorptionWhileDead(float amount) {
-        if (LoliExecutionManager.isTerminal((PlayerEntity) (Object) this)) {
-            return 0.0F;
-        }
-        return amount;
     }
 
     @Inject(method = "isCreativeLevelTwoOp", at = @At("HEAD"), cancellable = true)
